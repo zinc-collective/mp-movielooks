c4c3dca37ca8ae2c248015242ac07706fb25a332
Sean Clark Hess <seanhess@gmail.com>
2016-05-05 08:59:39 -0600

    modern bootstrap w/ app delegate and storyboard

---
+++ MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Asdf
+//
+//  Created by Sean Hess on 5/5/16.
+//  Copyright © 2016 Orbital Labs. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
